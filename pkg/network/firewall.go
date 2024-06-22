9a4a471ab9a3871ef729794472e99383ce00722a
Christian Chiarulli <chris.machine@pm.me>
2024-06-22 08:15:42 -0400

    firewall logs

--- pkg/network/firewall.go
+++ pkg/network/firewall.go
@@ -1,34 +1,31 @@
 package network
 
 import (
-	"fmt"
 	"log"
 	"os/exec"
+
+	"github.com/pterm/pterm"
 )
 
 // Function to configure the firewall
 func ConfigureFirewall() {
-	fmt.Println("Configuring firewall to allow HTTP (port 80) and HTTPS (port 443) traffic...")
-
+	spinner, _ := pterm.DefaultSpinner.Start("Configuring firewall to allow HTTP (port 80) and HTTPS (port 443) traffic...")
 	exec.Command("ufw", "enable").Run()
+	spinner.UpdateText("Firewall enabled successfully.")
 
 	// Allow HTTP and HTTPS traffic
 	err := exec.Command("ufw", "allow", "Nginx Full").Run()
+	spinner.UpdateText("Allowing Nginx Full...")
 	if err != nil {
 		log.Fatalf("Error allowing Nginx Full: %v", err)
 	}
 
 	// Reload the firewall to apply the changes
 	err = exec.Command("ufw", "reload").Run()
+	spinner.UpdateText("Reloading firewall...")
 	if err != nil {
 		log.Fatalf("Error reloading firewall: %v", err)
 	}
 
-	// Show the current firewall status
-	err = exec.Command("ufw", "status", "verbose").Run()
-	if err != nil {
-		log.Fatalf("Error showing firewall status: %v", err)
-	}
-
-	fmt.Println("Firewall configuration completed successfully.")
+	spinner.Success("Firewall configured successfully.")
 }
