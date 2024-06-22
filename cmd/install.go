64c35346fe1bcb5628f3b8aac072f7d01fad117e
Christian Chiarulli <chris.machine@pm.me>
2024-06-22 06:57:08 -0400

    fix

--- cmd/install.go
+++ cmd/install.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/nodetec/relaywiz/pkg/manager"
 	"github.com/nodetec/relaywiz/pkg/network"
@@ -12,56 +13,71 @@ import (
 )
 
 type model struct {
-	inputs    []string
+	textInput textinput.Model
 	cursor    int
-	input     string
+	inputs    []string
 	quitting  bool
+	err       error
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Relay Domain"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 30
+
+	return model{
+		textInput: ti,
+		inputs:    make([]string, 0),
+		quitting:  false,
+		err:       nil,
+	}
 }
 
 func (m model) Init() tea.Cmd {
-	return tea.Batch(tea.EnterAltScreen, tea.ClearScreen)
+	return textinput.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "enter":
-			m.inputs = append(m.inputs, m.input)
-			m.input = ""
-			if m.cursor < 1 {
+		switch msg.Type {
+		case tea.KeyEnter:
+			m.inputs = append(m.inputs, m.textInput.Value())
+			m.textInput.Reset()
+			if m.cursor == 0 {
+				m.textInput.Placeholder = "Email Address"
 				m.cursor++
 			} else {
 				return m, tea.Quit
 			}
-		case "backspace":
-			if len(m.input) > 0 {
-				m.input = m.input[:len(m.input)-1]
-			}
-		case "ctrl+c", "esc":
+		case tea.KeyCtrlC, tea.KeyEsc:
 			m.quitting = true
 			return m, tea.Quit
-		default:
-			m.input += msg.String()
 		}
+
+	// case errMsg:
+	// 	m.err = msg
+	// 	return m, nil
 	}
-	return m, nil
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	if m.quitting {
 		return "Exiting the program. Goodbye!\n"
 	}
-	var prompt string
-	switch m.cursor {
-	case 0:
-		prompt = fmt.Sprintf("Enter the relay domain name: %s", m.input)
-	case 1:
-		prompt = fmt.Sprintf("Enter the email address: %s", m.input)
-	default:
-		prompt = "Thank you! Proceeding with installation..."
-	}
-	return fmt.Sprintf("%s\n%s", prompt, "Use backspace to edit and enter to confirm.")
+
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.textInput.View(),
+		"(esc to quit)",
+	)
 }
 
 var installCmd = &cobra.Command{
@@ -69,8 +85,7 @@ var installCmd = &cobra.Command{
 	Short: "Install and configure the nostr relay",
 	Long:  `Install and configure the nostr relay, including package installation, nginx configuration, firewall setup, SSL certificates, and starting the relay service.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Create a new Bubble Tea program
-		p := tea.NewProgram(model{inputs: make([]string, 0)})
+		p := tea.NewProgram(initialModel())
 
 		// Run the Bubble Tea program to get user input
 		m, err := p.Run()
@@ -79,13 +94,12 @@ var installCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		// Check if the program was exited with ctrl+c or esc
 		if m.(model).quitting {
 			fmt.Println("Installation canceled.")
 			os.Exit(0)
 		}
 
-		// Extract the user input
+		// Extract the user inputs
 		inputs := m.(model).inputs
 		if len(inputs) < 2 {
 			fmt.Println("Error: Not enough input provided.")
