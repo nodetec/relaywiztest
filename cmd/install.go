94c2f9a26d24af3a4e2b86a94b02f9224a15056d
Christian Chiarulli <chris.machine@pm.me>
2024-06-22 07:15:48 -0400

    update

--- cmd/install.go
+++ cmd/install.go
@@ -1,112 +1,33 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/nodetec/relaywiz/pkg/manager"
 	"github.com/nodetec/relaywiz/pkg/network"
 	"github.com/nodetec/relaywiz/pkg/relay"
 	"github.com/spf13/cobra"
 )
 
-type model struct {
-	textInput textinput.Model
-	cursor    int
-	inputs    []string
-	quitting  bool
-	err       error
-}
-
-func initialModel() model {
-	ti := textinput.New()
-	ti.Placeholder = "Relay Domain"
-	ti.Focus()
-	ti.CharLimit = 156
-	ti.Width = 30
-
-	return model{
-		textInput: ti,
-		inputs:    make([]string, 0),
-		quitting:  false,
-		err:       nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textinput.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-			m.inputs = append(m.inputs, m.textInput.Value())
-			m.textInput.Reset()
-			if m.cursor == 0 {
-				m.textInput.Placeholder = "Email Address"
-				m.cursor++
-			} else {
-				return m, tea.Quit
-			}
-		case tea.KeyCtrlC, tea.KeyEsc:
-			m.quitting = true
-			return m, tea.Quit
-		}
-
-	// case errMsg:
-	// 	m.err = msg
-	// 	return m, nil
-	}
-
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	if m.quitting {
-		return "Exiting the program. Goodbye!\n"
-	}
-
-	return fmt.Sprintf(
-		"%s\n\n%s",
-		m.textInput.View(),
-		"(esc to quit)",
-	)
-}
-
 var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "Install and configure the nostr relay",
 	Long:  `Install and configure the nostr relay, including package installation, nginx configuration, firewall setup, SSL certificates, and starting the relay service.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		p := tea.NewProgram(initialModel())
+		reader := bufio.NewReader(os.Stdin)
 
-		// Run the Bubble Tea program to get user input
-		m, err := p.Run()
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-			os.Exit(1)
-		}
+		// Get the relay domain name
+		fmt.Print("Enter the relay domain name: ")
+		relayDomain, _ := reader.ReadString('\n')
+		relayDomain = strings.TrimSpace(relayDomain)
 
-		if m.(model).quitting {
-			fmt.Println("Installation canceled.")
-			os.Exit(0)
-		}
-
-		// Extract the user inputs
-		inputs := m.(model).inputs
-		if len(inputs) < 2 {
-			fmt.Println("Error: Not enough input provided.")
-			os.Exit(1)
-		}
-		relayDomain := inputs[0]
-		email := inputs[1]
+		// Get the email address
+		fmt.Print("Enter the email address: ")
+		email, _ := reader.ReadString('\n')
+		email = strings.TrimSpace(email)
 
 		fmt.Printf("Starting the installation and configuration of the nostr relay with domain: %s and email: %s...\n", relayDomain, email)
 
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.22.4
 require github.com/spf13/cobra v1.8.1
 
 require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbles v0.18.0 // indirect
@@ -14,8 +17,11 @@ require (
 	github.com/charmbracelet/x/input v0.1.0 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/gookit/color v1.5.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -24,10 +30,12 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pterm/pterm v0.12.79 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
